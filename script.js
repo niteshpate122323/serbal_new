3f0a13c9f4a75e848e5ef8371dc00abe97699e59
niteshpate122323 <niteshpatel.biz@gmail.com>
2024-11-16 12:45:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ document.addEventListener( 'DOMContentLoaded', function () {
         heightRatio: 0.4,
     } ).mount();
 } );
+
+
+    // // Function to handle scroll and add the 'visible' class to each card
+    // function onScroll() {
+    //     const cards = document.querySelectorAll('.card'); // Select all cards
+  
+    //     // Loop through all cards and check if each card is in the viewport
+    //     cards.forEach(card => {
+    //       const rect = card.getBoundingClientRect();
+  
+    //       // If the card is in the viewport (or almost)
+    //       if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+    //         card.classList.add('visible');
+    //       }
+    //     });
+    //   }
+  
+    //   // Listen for scroll event
+    //   window.addEventListener('scroll', onScroll);
+  
+    //   // Check on load in case the elements are already in the viewport
+    //   window.addEventListener('load', onScroll);
